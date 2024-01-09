6d0fd7e4966164ac420d06d5dfa7039e12b4d76b
Marcus Hanses <marcus.hanses@pax2pay.com>
2024-01-09 13:32:20 +0100

    Create empty summary.

--- mastercard/T140/Summary.ts
+++ mastercard/T140/Summary.ts
@@ -3,43 +3,39 @@ import { Cycle } from "./Cycle"
 import { Page } from "./Page"
 
 export interface Summary {
-	file: string
-	date: isoly.Date
-	run: isoly.Date
-	cycle: Cycle
-	total: { amount: Partial<Record<isoly.Currency, number>>; fee: Partial<Record<isoly.Currency, number>> }
+	file?: string
+	date?: isoly.Date
+	run?: isoly.Date
+	cycle?: Cycle
+	total?: { amount: Partial<Record<isoly.Currency, number>>; fee: Partial<Record<isoly.Currency, number>> }
 }
 
 export namespace Summary {
-	export function from(pages: Page[]): Summary | undefined {
-		let result: Omit<Summary, "file"> | undefined
+	export function from(pages: Page[]): Summary {
+		const result: Omit<Summary, "file"> = {}
 		let file: string | undefined
 		for (const page of pages) {
 			if (!Page.Failed.type.is(page)) {
-				!result &&
-					(result = (({ date, run, cycle }) => ({
-						date,
-						run,
-						cycle,
-						total: { amount: {}, fee: {} },
-					}))(page))
+				result.date ??= page.date
+				result.run ??= page.run
+				result.cycle ??= page.cycle
 				!file && "file" in page && (file = page.file)
 				page.class == "notification summary" && (result.total = add(result.total, page.content))
 			}
 		}
-		return file && result ? { ...result, file } : undefined
+		return file ? { ...result, file } : result
 	}
 	function add(total: Summary["total"], content: Page.NotificationSummary["content"]): Summary["total"] {
 		return content.reduce(
 			(r, e) => ({
 				...r,
 				amount: {
-					...r.amount,
-					[e.currency]: isoly.Currency.add(e.currency, r.amount?.[e.currency] ?? 0, e.total.amount),
+					...r?.amount,
+					[e.currency]: isoly.Currency.add(e.currency, r?.amount?.[e.currency] ?? 0, e.total.amount),
 				},
 				fee: {
-					...r.fee,
-					[e.currency]: isoly.Currency.add(e.currency, r.fee[e.currency] ?? 0, -e.total.fee),
+					...r?.fee,
+					[e.currency]: isoly.Currency.add(e.currency, r?.fee[e.currency] ?? 0, -e.total.fee),
 				},
 			}),
 			total
