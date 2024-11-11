635c6156ae69e3fae028f031f9e09cd0e2e8675d
SiFalorSum <nilshansander@hotmail.com>
2024-11-11 17:08:19 +0100

    remove comment

--- diners/index.ts
+++ diners/index.ts
@@ -1,11 +1,1 @@
 export { InterchangeIssuerDetail } from "./InterchangeIssuerDetail"
-
-/* 
-DCI processing time 10:00 GMT mon-fri
-
-An exception to the above rule is when a Settlement Currency holiday prevents settlement on a certain date. This
-is where there is a holiday which affects the transfer of funds in a particular Settlement Currency and the banks
-cannot settle in that currency on that date. These currency holidays will be handled automatically by the
-Interchange settlement process in conjunction with the Settlement Agency and Recaps affected by the currency
-holiday will be settled on the next Business Day.
-*/
